84d619bebc95f936b6527b09099aa093fb0375bc
sumit-BO <sumit.b@betteropinions.in>
2022-08-16 11:12:53 +0530

    initial commit

---
+++ BO_BE_KO/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BO_BE_KO project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BO_BE_KO.settings')
+
+application = get_asgi_application()
